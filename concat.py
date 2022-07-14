12257ca69ab5e77f0ffd56041a7c25f1a87bf8ca
Nayana2410 <108855925+Nayana2410@users.noreply.github.com>
2022-07-14 13:59:58 +0530

    Add files via upload

---
+++ concat.py
@@ -0,0 +1,21 @@
+#wpp to  perform concatanation &repation on string
+
+#concatanation
+f_name ='Nayana'
+m_name ='Sukhadeo'
+l_name ='Lokhande'
+
+#method1
+print(f_name,m_name,l_name)
+
+#method2
+print(f_name+""+m_name+""+l_name)
+
+#method3
+print(f'hello,{l_name} {f_name} how are you?')
+
+#method4
+print("hi {} {}, how  are you?".format(f_name,l_name))
+
+#repeation
+print(f_name*4)
